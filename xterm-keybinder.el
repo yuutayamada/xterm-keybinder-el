95583f70682b421eb7e11a0c8c43a6bfcc39b90e
Yuta Yamada <cokesboy@gmail.com>
2015-08-03 16:22:34 -0700

    Refactor; use let-alist

--- xterm-keybinder.el
+++ xterm-keybinder.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2015  Yuta Yamada
 
 ;; Author: Yuta Yamada <cokesboy"at"gmail.com>
-;; Package-Requires: ((emacs "24.3") (cl-lib "0.5"))
+;; Package-Requires: ((emacs "24.3") (cl-lib "0.5") (let-alist "1.0.1"))
 ;; Version: 0.1.0
 ;; Keywords: Convenient
 
@@ -139,19 +139,27 @@ Use standard US layout.  See also https://en.wikipedia.org/wiki/IBM_PC_keyboard.
     (?\} . "braceright")
     (?~  . "asciitilde")))
 
-(defun xterm-keybinder-get-modifier (symbol)
-  "Return precise modifier string from SYMBOL."
-  (cl-case symbol
-    (S     "Shift ~Ctrl ~Alt ~Super ~Hyper") ; Shift
-    (C     "Ctrl ~Shift ~Alt ~Super ~Hyper") ; Control
-    (s     "Super ~Ctrl ~Alt ~Shift ~Hyper") ; Super
-    (H     "Hyper ~Ctrl ~Alt ~Shift ~Super") ; Hyper
-    (C-S   "Ctrl Shift  ~Alt ~Super ~Hyper")
-    (C-M   "Ctrl Alt ~Shift  ~Super ~Hyper")
-    (C-M-S "Ctrl Alt  Shift  ~Super ~Hyper")
-    (M-S   "Alt Shift ~Ctrl ~Super ~Hyper")
-    (s-S   "Super %s~Alt ~Ctrl ~Hyper")
-    (H-S   "Hyper %s~Alt ~Ctrl ~Super")))
+(defconst xterm-keybinder-table
+  '((S     . ((mod    . "Shift ~Ctrl ~Alt ~Super ~Hyper")
+              (suffix . "0x53")))
+    (C     . ((mod    . "Ctrl ~Shift ~Alt ~Super ~Hyper")
+              (suffix . "0x63")))
+    (s     . ((mod    . "Super ~Ctrl ~Alt ~Shift ~Hyper")
+              (suffix . "0x73")))
+    (H     . ((mod    . "Hyper ~Ctrl ~Alt ~Shift ~Super")
+              (suffix . "0x68")))
+    (C-S   . ((mod    . "Ctrl Shift  ~Alt ~Super ~Hyper")
+              (spacer . "")))
+    (C-M   . ((mod    . "Ctrl Alt ~Shift  ~Super ~Hyper")
+              (spacer . "===")))
+    (C-M-S . ((mod    . "Ctrl Alt  Shift  ~Super ~Hyper")
+              (spacer . "=")))
+    (M-S   . ((mod    . "Alt Shift ~Ctrl ~Super ~Hyper")
+              (spacer . "==")))
+    (s-S   . ((mod    . "Super %s~Alt ~Ctrl ~Hyper")
+              (spacer . "====")))
+    (H-S   . ((mod    . "Hyper %s~Alt ~Ctrl ~Super")
+              (spacer . "=====")))))
 
 (defun xterm-keybinder-get-desc (keydef)
   "Return pair of key and modifier from KEYDEF."
@@ -168,7 +176,9 @@ Use standard US layout.  See also https://en.wikipedia.org/wiki/IBM_PC_keyboard.
          (char (car pair))
          (no-shift (car (assoc char xterm-keybinder-key-pairs)))
          (shifted  (cdr (rassoc char xterm-keybinder-key-pairs)))
-         (mod (xterm-keybinder-get-modifier (intern-soft (mapconcat 'string (cdr pair) "-")))))
+         (mod
+          (assoc-default 'mod (assoc-default (intern-soft (mapconcat 'string (cdr pair) "-"))
+                                             xterm-keybinder-table))))
     (format "%s <KeyPress> %s: %s"
             (if shifted
                 ;; Shifted key like C-+, hides no-shift key (in this case, C-=).
@@ -207,48 +217,42 @@ escape sequence.")
   (format "string(\"\\033[%c%%s\")" xterm-keybinder-private-char))
 (defconst xterm-keybinder-C-char-list
   '(":" ";" "," "." "'" "0" "1" "2" "3" "4" "5" "6" "7" "8" "9"))
-(defconst xterm-keybinder-table
-  '((C-S   . "")
-    (C-M   . "===")
-    (C-M-S . "=")
-    (M-S   . "==")
-    (s-S   . "====")
-    (H-S   . "=====")))
 
 ;;;###autoload
 (defun xterm-keybinder-setup ()
   "Enable Emacs keybinds even in the xterm terminal Emacs."
   (interactive)
-  (let ((prefix xterm-keybinder-prefix)
-        (map input-decode-map)
-        (cs  (assoc-default 'C-S   xterm-keybinder-table))
-        (cm  (assoc-default 'C-M   xterm-keybinder-table))
-        (cms (assoc-default 'C-M-S xterm-keybinder-table))
-        (ms  (assoc-default 'M-S   xterm-keybinder-table))
-        (sS  (assoc-default 's-S   xterm-keybinder-table))
-        (hS  (assoc-default 'H-S   xterm-keybinder-table)))
-    ;; C-S-[a-z], C-M-[a-z] and C-M-S-[a-z]
-    (cl-loop with defkey = (lambda (props)
-                             (cl-loop for (mod c key) in props do
-                                      (define-key map (format "%s%s%c" prefix mod c) key)))
-             for c from ?\s to ?~
-             for char = "SPC" then (downcase (char-to-string c))
-             for C-S-key   = (kbd (concat "C-S-"   char))
-             for C-M-key   = (kbd (concat "C-M-"   char))
-             for C-M-S-key = (kbd (concat "C-M-S-" char))
-             for M-S-key   = (kbd (concat "M-S-"   char))
-             for s-S-key   = (kbd (concat "s-S-"   char))
-             for H-S-key   = (kbd (concat "H-S-"   char))
-             if (eq c ?=) do '() ; just ignore
-             else if (<= ?A c ?Z) do
-             (funcall defkey `((,cs ,c ,C-S-key) (,cms ,c ,C-M-S-key) (,ms ,c ,M-S-key)
-                               (,sS ,c ,s-S-key) (,hS ,c ,H-S-key)))
-             else if (<= ?a c ?z) do
-             (funcall defkey `((,cm ,c ,C-M-key)))
-             else if (eq c ?\s) do ; for space
-             (funcall defkey `((,cs ,c ,C-S-key) (,cm ,c ,C-M-key) (,cms ,c ,C-M-S-key)))
-             else
-             do (funcall defkey `((,hS ,c ,H-S-key) (,sS ,c ,s-S-key))))))
+  (let-alist xterm-keybinder-table
+    (let ((prefix xterm-keybinder-prefix)
+          (map input-decode-map)
+          (cs  .C-S.spacer)
+          (cm  .C-M.spacer)
+          (cms .C-M-S.spacer)
+          (ms  .M-S.spacer)
+          (sS  .s-S.spacer)
+          (hS  .H-S.spacer))
+      ;; C-S-[a-z], C-M-[a-z] and C-M-S-[a-z]
+      (cl-loop with defkey = (lambda (props)
+                               (cl-loop for (mod c key) in props do
+                                        (define-key map (format "%s%s%c" prefix mod c) key)))
+               for c from ?\s to ?~
+               for char = "SPC" then (downcase (char-to-string c))
+               for C-S-key   = (kbd (concat "C-S-"   char))
+               for C-M-key   = (kbd (concat "C-M-"   char))
+               for C-M-S-key = (kbd (concat "C-M-S-" char))
+               for M-S-key   = (kbd (concat "M-S-"   char))
+               for s-S-key   = (kbd (concat "s-S-"   char))
+               for H-S-key   = (kbd (concat "H-S-"   char))
+               if (eq c ?=) do '() ; just ignore
+               else if (<= ?A c ?Z) do
+               (funcall defkey `((,cs ,c ,C-S-key) (,cms ,c ,C-M-S-key) (,ms ,c ,M-S-key)
+                                 (,sS ,c ,s-S-key) (,hS ,c ,H-S-key)))
+               else if (<= ?a c ?z) do
+               (funcall defkey `((,cm ,c ,C-M-key)))
+               else if (eq c ?\s) do ; for space
+               (funcall defkey `((,cs ,c ,C-S-key) (,cm ,c ,C-M-key) (,cms ,c ,C-M-S-key)))
+               else
+               do (funcall defkey `((,hS ,c ,H-S-key) (,sS ,c ,s-S-key)))))))
 
 (defun xterm-keybinder-insert ()
   "Insert configuration for XTerm.
@@ -321,18 +325,14 @@ You can use this to insert xterm configuration by yourself."
 (defun xterm-keybinder-make-base-format (sym)
   ;; See also ‘event-apply-XXX-modifier’
   (let ((C-x@ "string(0x18) string(0x40)"))
-    (format "  %s <KeyPress> %%s: %s string(0x%%x) \\n\\"
-            (xterm-keybinder-get-modifier sym)
-            (if (member sym '(S C s H))
-                ;; event modifier
-                (format "%s string(%s)" C-x@ (cl-case sym
-                                               (S "0x53")
-                                               (C  "0x63")
-                                               (s "0x73")
-                                               (H "0x68")))
-              ;; \033[=
-              (format xterm-keybinder-format
-                      (assoc-default sym xterm-keybinder-table))))))
+    (let-alist (assoc-default sym xterm-keybinder-table)
+      (format "  %s <KeyPress> %%s: %s string(0x%%x) \\n\\"
+              .mod
+              (if (member sym '(S C s H))
+                  ;; event modifier
+                  (format "%s string(%s)" C-x@ .suffix)
+                ;; \033[=
+                (format xterm-keybinder-format .spacer))))))
 
 ;; For debug
 ;;  (message (key-description (read-key-sequence "input: ")))
